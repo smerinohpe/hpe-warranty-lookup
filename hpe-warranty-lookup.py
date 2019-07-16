be7caadc987971315bba44a2548ae45a14dde06b
Santiago Merino <48120463+smerinohpe@users.noreply.github.com>
2019-07-16 14:13:50 +0200

    Update hpe-warranty-lookup.py
    Now, check if the serial exist or not

--- hpe-warranty-lookup.py
+++ hpe-warranty-lookup.py
@@ -9,7 +9,7 @@ def get_warranty_HTML(serial):
     params = urllib.parse.urlencode({'rows[0].item.serialNumber': serial})
     headers = {"Content-type": "application/x-www-form-urlencoded"}
 
-    conn = http.client.HTTPConnection("h20564.www2.hpe.com")
+    conn = http.client.HTTPSConnection("support.hpe.com")
     conn.request("POST", "/hpsc/wc/public/find", params, headers)
     response = conn.getresponse()
     
@@ -37,10 +37,16 @@ def extract_warranty_info(html):
 
 def main(argv):
     if len(argv) < 2:
-        sys.exit("ERROR: A valid HPE Serial Number must be specified as an argument to this script")
+        print("ERROR: A valid HPE Serial Number must be specified as an argument to this script")
+        argv = [0, input('Please insert your serial: ')]
 
+    print('\nChecking for %s ... \n' % (argv[1]))
     warranty_html = get_warranty_HTML(argv[1])
-    print(extract_warranty_info(warranty_html))
+    warranty_txt = extract_warranty_info(warranty_html)
+    if not warranty_txt:
+        warranty_txt = "Serial not found or Expired"
+    print('%s\n\nChecked!!!\n' % (warranty_txt))
+    wait = input('Press ENTER to continue.')
  
 if __name__ == "__main__":
     main(sys.argv)
